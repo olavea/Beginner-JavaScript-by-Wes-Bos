b92cfb35fe3c3b2cf552eb5c20a72ad2b7d83c54
olavea <ola@olavea.com>
2025-03-13 13:22:18 +0100

    function custom #13

---
+++ playground/custom-functions/ways-to-make-a-function.js
@@ -0,0 +1,14 @@
+
+
+function calculateBill() {
+    // this body
+    console.log("it");
+    const total = 100 * 1.13;
+    return total;
+};
+
+const myToT = calculateBill()
+
+console.log("it", myToT);
+
+// type allow pasting 
