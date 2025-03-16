1e6a5160231c834d342ea23606b17852a94a7350
olavea <ola@olavea.com>
2025-03-16 19:00:48 +0100

    #13

--- playground/custom-functions/ways-to-make-a-function.js
+++ playground/custom-functions/ways-to-make-a-function.js
@@ -1,14 +1,19 @@
 
 
-function calculateBill() {
+
+
+function calculateBill(billAmount, taxRate) {
     // this body
     console.log("it");
-    const total = 100 * 1.13;
+    const total = billAmount * (1 + taxRate) ;
     return total;
 };
+// fun call or run
+const myToT = calculateBill();
 
-const myToT = calculateBill()
 
-console.log("it", myToT);
+const myToT2 = calculateBill();
+
+console.log("it", myToT, myToT2);
 
 // type allow pasting 
