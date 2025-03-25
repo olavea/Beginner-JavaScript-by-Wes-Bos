ba8e92f9a81bc498f04255f285a2c1f95189afbd
olavea <ola@olavea.com>
2025-03-25 17:47:10 +0100

    classes round

---
+++ exercises/20 - The DOM/creating.js
@@ -0,0 +1,74 @@
+
+
+
+const pic = document.querySelector('.nice');
+
+pic.classList.add('open');
+pic.classList.remove('open');
+pic.classList.toggle('round');
+console.log(pic.classList);
+
+
+// const p = document.querySelector('p');
+// const d =document.querySelectorAll('d')
+// const divs = document.querySelectorAll('div');
+// const item2 = document.querySelector('.item2');
+// const item2image = item2.querySelector('img');
+// // console.log(p);
+
+
+// // NodeList a list of things, no methods buuilt into it, so not an array
+
+// const heading = document.querySelector('h2');
+
+// console.log(heading.textContent);
+
+// heading.textContent = 'wes is cool'
+
+// console.log(heading.textContent);
+
+
+
+
+
+
+
+
+
+// const pizzaList = document.querySelector('.pizza');
+
+// pizzaList.textContent = `${pizzaList.textContent} 🍕`;
+
+// pizzaList.insertAdjacentText('afterbegin', '🍕');
+
+// // pizzaList.insertAdjacentText('bedoreend', '🍕');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
